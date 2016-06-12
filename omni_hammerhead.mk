f4ec9aff3d2cbab53d05c87cd45879dbcc802ceb
cherifyass <cherif.esir@gmail.com>
2016-06-12 14:46:12 +0200

    hammerhead: update to MOB30M
    Android 6.0.1 Release 46
    
    Change-Id: I320a69faafa37205b0097ae0713e21f9b5390679

--- omni_hammerhead.mk
+++ omni_hammerhead.mk
@@ -43,9 +43,9 @@ PRODUCT_MANUFACTURER := LGE
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=hammerhead \
-    BUILD_FINGERPRINT=google/hammerhead/hammerhead:6.0.1/MOB30H/2751534:user/release-keys \
-    PRIVATE_BUILD_DESC="hammerhead-user 6.0.1 MOB30H 2751534 release-keys" \
-    BUILD_ID=MOB30H
+    BUILD_FINGERPRINT=google/hammerhead/hammerhead:6.0.1/MOB30M/2862625:user/release-keys \
+    PRIVATE_BUILD_DESC="hammerhead-user 6.0.1 MOB30M 2862625 release-keys" \
+    BUILD_ID=MOB30M
 
 # Kernel inline build
 TARGET_KERNEL_SOURCE := kernel/lge/hammerhead
